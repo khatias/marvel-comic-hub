8a74c6e26413defc1abc9dba0d414f3b3309b8cf
khatia <130936157+khatias@users.noreply.github.com>
2025-01-06 19:10:29 -0500

    feat: Implement hero section UI for mobile responsiveness

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import Header from './components/header/Header';
+import Hero from './components/heroSection/Hero';
 
 function App() {
   return (
     <>
     <Header/>
+    <Hero/>
+    
     </>
   );
 }
